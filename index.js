d25ccaa66c340ddb59e4d97cc18a58cdab1d492b
Farhan <franjha58@icloud.com>
2022-09-11 13:05:19 +0500

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+function generateAlert() {
+    alert("Submitted");
+}
+
+function resetOnClick() {
+    document.getElementsByClassName("myform").reset();
+}
+
+function colorPink() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "pink";
+}
+
+function colorWhite() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+}
